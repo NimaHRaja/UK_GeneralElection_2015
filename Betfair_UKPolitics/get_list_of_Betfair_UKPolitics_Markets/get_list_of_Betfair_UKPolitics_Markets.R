40a666fb81f4615c266ae0bb107a50623392f067
nimahraja <nimaradja@gmail.com>
2015-01-06 17:09:10 +0000

    get list of (83) Betfair UKpolitics Markets

---
+++ Betfair_UKPolitics/get_list_of_Betfair_UKPolitics_Markets/get_list_of_Betfair_UKPolitics_Markets.R
@@ -0,0 +1,56 @@
+List_of_SubMarkets <- function(url, output){
+    
+    List_of_SubLinks <- function(url){
+        parsed_html <- htmlTreeParse(url,useInternal=TRUE)
+        xpathSApply(parsed_html,
+                    "//ul[@class = 'children']/li/a",
+                    xmlGetAttr,"href")       
+    }
+    
+    
+    if (!grepl("MENU", url)) {
+        
+        output <- rbind(output, url)
+        
+    } else {
+        
+        type_id <- substr(url,1,1)
+        
+        if(type_id == "?")
+            
+            sub_url <- paste(base, url, sep = "/")
+        
+        else 
+            
+            sub_url <- url
+        
+        
+        sub_links <- List_of_SubLinks(sub_url)
+        
+        for (sub_link in sub_links){
+            output <- List_of_SubMarkets(sub_link, output)
+        }
+        
+    }
+    output
+}
+
+
+library(XML)
+
+base <- "http://www.betfair.com/exchange"
+output <- list()
+
+
+
+url <-"http://www.betfair.com/exchange/?nodeId=MENU:2707982"
+output <- List_of_SubMarkets(url, output)
+
+output <- paste("http://www.betfair.com",unlist(output), sep = "")
+
+
+write(output, "list_of_Betfair_UKPolitics_Markets.txt")
+
+
+
+output
