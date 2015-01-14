0755b5c78875b7a21d3f04b7368db9d34f8bc48a
nimahraja <nimaradja@gmail.com>
2015-01-14 19:07:27 +0000

    very very very draft version
    chull of 3-outcome markets

--- Betfair_UKPolitics/Analysing Data/Analyse_UKPolitics_Odds_Data.R
+++ Betfair_UKPolitics/Analysing Data/Analyse_UKPolitics_Odds_Data.R
@@ -4,27 +4,59 @@ all_odds_data <- read.table("../Betfair_UKPolitics_Odds_History.csv",
                             , stringsAsFactor = FALSE)
 
 
-max(all_odds_data$Matched, na.rm = TRUE)
-
-View(all_odds_data[all_odds_data$Matched>100000 & !is.na(all_odds_data$Matched),])
-
-
-
 
 major_markets <- 
-    unique(all_odds_data[all_odds_data$Matched>100000 & !is.na(all_odds_data$Matched),]$Market)
+    unique(all_odds_data[all_odds_data$Matched>10000 & !is.na(all_odds_data$Matched),]$Market)
 
 
 
 major_markets_data <- all_odds_data[all_odds_data$Market %in% major_markets,]
 
-library(ggplot2)
 
-qplot(
-    strptime(date_char, "%d/%m/%Y %H:%M:%s"),1/Back,
-    data = 
-        subset(major_markets_data, 
-               Market == "2015 UK General Election - Most Seats")
-    , color = Outcome
-    )
+test_data <- major_markets_data[c(203,201,204,202),3:5]
+row.names(test_data) <- NULL
 
+test_data[1,2:3] <- 1/(1/test_data[1,2:3] + 1/test_data[3,2:3])
+
+test_data <- test_data[c(1,2,4),]
+
+
+
+
+get_chull <- function(data){
+    
+    get_possible_points <- function(prob){
+        
+        
+        points <- expand.grid(unlist(prob[1,]), unlist(prob[2,]))
+        
+        points <- cbind(points, Var3 = (1- rowSums(points)))
+        
+        points[(points[, 3] < prob[3,1]) & (points[, 3] > prob[3,2]),]
+        
+    }
+    
+    prob <- 1/test_data[,2:3]
+    points <- get_possible_points(prob)
+    
+    
+    points2 <- get_possible_points(prob[c(1,3,2),])
+    names(points2) <- c("Var1", "Var3", "Var2")
+    points <- rbind(points, points2)
+    
+    
+    points3 <- get_possible_points(prob[c(3,2,1),])
+    names(points3) <- c("Var3", "Var2", "Var1")
+    points <- rbind(points, points3)
+    
+    
+  
+    points
+    
+}
+
+
+
+
+points <- get_chull(test_data)
+row.names(points ) <- NULL
