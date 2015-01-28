a13319c026d3f64a2b49b02aa7bf48ea69f032d3
nimahraja <nimaradja@gmail.com>
2015-01-28 15:47:24 +0000

    New data + new output

--- Betfair_UKPolitics/Analysing Data/Single_Market_Analysis.R
+++ Betfair_UKPolitics/Analysing Data/Single_Market_Analysis.R
@@ -9,7 +9,8 @@ Load_Libraries(c("ggplot2", "ggtern", "reshape2", "stringr"))
 ############################################################################
 # Subset Market?
 
-this_Market <- "UK Seat Totals - UKIP Seats Total"
+# this_Market <- "UK Seat Totals - UKIP Seats Total"
+this_Market <- "2015 UK General Election - Overall Majority"
 
 data <- subset(all_odds_data, Market == this_Market)
 
@@ -19,19 +20,22 @@ row.names(data) <- NULL
 ############################################################################
 # Market's data -> CSV
 
-Single_Market_CSV_Output(data, "UKIP_Seats_Market_Ternary.csv")
+# Single_Market_CSV_Output(data, "UKIP_Seats_Market_Ternary.csv")
+Single_Market_CSV_Output(data, "Majority_Market_Ternary.csv")
 
 
 ############################################################################
 # Class Graph -> PNG
 
-Single_Market_Classic_Graph_Output(data, "UKIP_Seats_Market.png")
+# Single_Market_Classic_Graph_Output(data, "UKIP_Seats_Market.png")
+Single_Market_Classic_Graph_Output(data, "Majority_Market.png")
 
 
 ############################################################################
 # Ternary Animated -> Gif
 
-GIF_Output  <- "UKIP_Seats_Prob.gif"
+# GIF_Output  <- "UKIP_Seats_Prob.gif"
+GIF_Output  <- "Majority_Prob.gif"
 
 number_of_diagrams <- 200
 delay <- 5
@@ -39,10 +43,13 @@ delay <- 5
 first_date <-strptime("2014-07-01 00:00:00", "%Y-%m-%d %H:%M:%s")
 last_date <-strptime("2015-01-30 00:00:00", "%Y-%m-%d %H:%M:%s")
 
-main_outputs <- list("None", "One to Five")
-other_outputs <- "Over Five"
+# main_outputs <- list("None", "One to Five"); other_outputs <- "Over Five";
+main_outputs <- list("Conservative Majority", "Labour Majority"); 
+other_outputs <- "No Majority";
 
-labels <- c("None", "One to Five",  "Over 5")
+# labels <- c("None", "One to Five",  "Over 5")
+labels <- c("CON", "LAB", "NO")
+
 
 Single_Market_Ternary_Animated_Output(data, 
                                       first_date, 
@@ -51,3 +58,5 @@ Single_Market_Ternary_Animated_Output(data,
                                       other_outputs, 
                                       GIF_Output, 
                                       labels)
+
+write(as.character(Sys.time()), "time.txt")
