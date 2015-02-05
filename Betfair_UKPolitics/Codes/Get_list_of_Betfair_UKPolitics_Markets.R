12664abb0a873e67b95e038294df4fedc6be01eb
nimahraja <nimaradja@gmail.com>
2015-02-05 18:06:27 +0000

    Major re-organisation-1
    Get_list

---
+++ Betfair_UKPolitics/Codes/Get_list_of_Betfair_UKPolitics_Markets.R
@@ -0,0 +1,19 @@
+source("../Functions/Load_Libraries.R")
+source("../Functions/List_of_SubMarkets.R")
+
+Load_Libraries("XML")
+
+
+
+base <- "http://www.betfair.com/exchange"
+output <- list()
+
+
+
+url <-"http://www.betfair.com/exchange/?nodeId=MENU:2707982"
+output <- List_of_SubMarkets(url, output)
+
+output <- paste("http://www.betfair.com",unlist(output), sep = "")
+
+
+write(output, "../data/list_of_Betfair_UKPolitics_Markets.txt")
--- Betfair_UKPolitics/Functions/List_of_SubMarkets.R
+++ Betfair_UKPolitics/Functions/List_of_SubMarkets.R
@@ -34,20 +34,3 @@ List_of_SubMarkets <- function(url, output){
     }
     output
 }
-
-
-source("../../General_Functions/Load_Libraries.R")
-Load_Libraries("XML")
-
-base <- "http://www.betfair.com/exchange"
-output <- list()
-
-
-
-url <-"http://www.betfair.com/exchange/?nodeId=MENU:2707982"
-output <- List_of_SubMarkets(url, output)
-
-output <- paste("http://www.betfair.com",unlist(output), sep = "")
-
-
-write(output, "../data/list_of_Betfair_UKPolitics_Markets.txt")
