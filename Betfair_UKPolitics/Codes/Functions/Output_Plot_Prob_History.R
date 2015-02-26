6a567eced20641ed37836557f281aac685dd8847
NimaHRaja <nimaradja@gmail.com>
2015-02-26 15:54:36 +0000

    Renamed + Merged

--- Betfair_UKPolitics/Codes/Functions/Output_Plot_Prob_History.R
+++ Betfair_UKPolitics/Codes/Functions/Output_Plot_Prob_History.R
@@ -1,4 +1,4 @@
-Get_Prob_History_Plot <- function(market, errorbar)
+Output_Plot_Prob_History <- function(market, errorbar)
 {
     
     data <- all_odds_data[all_odds_data$Market %in% market ,]
@@ -20,3 +20,18 @@ Get_Prob_History_Plot <- function(market, errorbar)
     
     plot
 }
+
+# 
+# Single_Market_Classic_Graph_Output <- function(data, PNG_Output){
+#     
+#     png(PNG_Output, width = 800, height = 400)
+#     
+#     print(
+#         qplot(date, 
+#               100/Back, 
+#               data = data, 
+#               color = Outcome, 
+#               ylab = "(implied) probability (%)"))
+#     
+#     dev.off()
+# }
